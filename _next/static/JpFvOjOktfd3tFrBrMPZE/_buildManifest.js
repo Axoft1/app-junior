750fc8a3c57f589a79786d4aeb74c26bf3ba7e23
Aleksei Anufriev <axoft10@gmail.com>
2023-04-04 21:48:52 +0700

    Deploy gh-pages

--- _next/static/JpFvOjOktfd3tFrBrMPZE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/870-5efcfa76da8bfdf7.js","static/css/bda9bdd8ec21ae7f.css","static/chunks/pages/index-e86499846ae45b34.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
