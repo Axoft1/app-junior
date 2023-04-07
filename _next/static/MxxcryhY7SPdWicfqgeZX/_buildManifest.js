db6ce85bfc6752d810d91e8ae2f1face8b451f14
Aleksei Anufriev <axoft10@gmail.com>
2023-04-07 09:39:55 +0700

    Deploy gh-pages

--- _next/static/MxxcryhY7SPdWicfqgeZX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/870-5efcfa76da8bfdf7.js","static/css/dba6f738c76ac00f.css","static/chunks/pages/index-c8eafac75ef6577f.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/XC4xcg46kYqZJOyopaLhn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/870-5efcfa76da8bfdf7.js","static/css/dba6f738c76ac00f.css","static/chunks/pages/index-83036359dbbc770b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
